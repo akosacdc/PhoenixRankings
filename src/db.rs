293760b8443e98933b2c23a603bff8072967f30e
Akos Vezsenyi <akos.vezsenyi@accenture.com>
2024-10-14 15:00:43 +0300

    tmp

--- src/db.rs
+++ src/db.rs
@@ -1,37 +1,102 @@
 use skillratings::glicko::GlickoRating;
+use skillratings::Outcomes;
 
 pub struct PlayersDatabase {
-	list: Vec<(String, GlickoRating)>,
+	list: Vec<(String, GlickoRating, Vec<(GlickoRating, Outcomes)>)>,
 }
 
 impl PlayersDatabase {
-	pub fn new(in_list: Vec<(String, GlickoRating)>) -> PlayersDatabase {
+	pub fn new(in_list: Vec<(String, GlickoRating, Vec<(GlickoRating, Outcomes)>)>) -> PlayersDatabase {
 		PlayersDatabase{ list: in_list }
 	}
 
 	pub fn add_new(&mut self, name: String) {
-		self.list.push((name, GlickoRating::new()));
+		self.list.push((name, GlickoRating::new(), vec![]));
 	}
 
-	pub fn add_existing(&mut self, name: String, rating: GlickoRating) {
-		self.list.push((name, rating));
-	}
+	// pub fn add_existing(&mut self, name: String, rating: GlickoRating) {
+	// 	self.list.push((name, rating));
+	// }
 
-	pub fn list(&self) -> Vec<(String, GlickoRating)> {
+	pub fn list(&self) -> Vec<(String, GlickoRating, Vec<(GlickoRating, Outcomes)>)> {
 		self.list.clone()
 	}
 
 	pub fn find_player(&self, name: String) -> bool {
-		for (db_name, _) in &self.list {
+		for (db_name, _, _) in &self.list {
 			if *db_name == name { return true; }
 		}
 		false
 	}
 
 	pub fn get_player_data(&self, name: String) -> Option<GlickoRating> {
-		for (db_name, glicko_rating) in &self.list {
+		for (db_name, glicko_rating, _) in &self.list {
 			if *db_name == name { return Some(*glicko_rating); }
 		}
 		None
+	}
+
+	pub fn add_new_result(&mut self, name: String, opponent: GlickoRating, outcome: Outcomes) {
+		for (db_name, _, results) in &mut self.list {
+			if *db_name == name { 
+				(*results).push((opponent, outcome));
+			}
+		}
+	}
+}
+
+pub struct TeamsDatabase {
+	list: Vec<(String, String, GlickoRating, Vec<(GlickoRating, Outcomes)>)>,
+}
+
+impl TeamsDatabase {
+	pub fn new(in_list: Vec<(String, String, GlickoRating, Vec<(GlickoRating, Outcomes)>)>) -> TeamsDatabase {
+		TeamsDatabase{ list: in_list }
+	}
+
+	pub fn add_new(&mut self, player1_name: String, player1_glicko: GlickoRating, player2_name: String, player2_glicko: GlickoRating) {
+		let team_rating = (player1_glicko.rating + player2_glicko.rating) / 2.0;
+		let team_deviation = ((player1_glicko.deviation * player1_glicko.deviation) + (player2_glicko.deviation * player2_glicko.deviation)).sqrt();
+		let team_glicko = GlickoRating {
+			rating: team_rating,
+			deviation: team_deviation,
+		};
+
+		self.list.push((player1_name, player2_name, team_glicko, vec![]));
+	}
+
+	// pub fn add_existing(&mut self, name: String, rating: GlickoRating) {
+	// 	self.list.push((name, rating));
+	// }
+
+	pub fn list(&self) -> Vec<(String, String, GlickoRating, Vec<(GlickoRating, Outcomes)>)> {
+		self.list.clone()
+	}
+
+	pub fn find_team(&self, player1_name: String, player2_name: String) -> bool {
+		for (db_name1, db_name2, _, _) in &self.list {
+			if *db_name1 == player1_name && *db_name2 == player2_name { return true; }
+			if *db_name2 == player1_name && *db_name1 == player2_name { return true; }
+		}
+		false
+	}
+
+	pub fn get_team_glicko(&self, player1_name: String, player2_name: String) -> Option<GlickoRating> {
+		for (db_name1, db_name2, glicko, _) in &self.list {
+			if *db_name1 == player1_name && *db_name2 == player2_name { return Some(*glicko); }
+			if *db_name2 == player1_name && *db_name1 == player2_name { return Some(*glicko); }
+		}
+		None
+	}
+
+	pub fn add_new_result(&mut self, player1_name: String, player2_name: String, opponent: GlickoRating, outcome: Outcomes) {
+		for (db_name1, db_name2, _, results) in &mut self.list {
+			if *db_name1 == player1_name && *db_name2 == player2_name { 
+				(*results).push((opponent, outcome));
+			}
+			if *db_name2 == player1_name && *db_name1 == player2_name {
+				(*results).push((opponent, outcome));
+			}
+		}
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,42 @@ use std::io;
 use std::io::Write;
 use inquire::Select;
 use inquire::InquireError;
-use skillratings::glicko::GlickoRating;
+use skillratings::{
+    glicko::{glicko_rating_period, GlickoConfig, GlickoRating},
+    Outcomes,
+};
+use csv::Writer;
+use csv::Reader;
 
 mod db;
 use crate::db::PlayersDatabase;
+use crate::db::TeamsDatabase;
 
 fn list_all_players(players_database: &PlayersDatabase) {
     let players_list = players_database.list();
-    for (name, glicko) in players_list {
+    for (name, glicko, results) in players_list {
         println!("{} {} {}", name, glicko.rating, glicko.deviation);
+        for (opponent, outcome) in results {
+            if outcome == Outcomes::WIN {
+                println!("      {} {} win", opponent.rating, opponent.deviation);
+            } else {
+                println!("      {} {} loss", opponent.rating, opponent.deviation);
+            }
+        }
+    }
+}
+
+fn list_all_teams(teams_database: &TeamsDatabase) {
+    let teams_list = teams_database.list();
+    for (player1_name, player2_name, glicko, results) in teams_list {
+        println!("{} {} {} {}", player1_name, player2_name, glicko.rating, glicko.deviation);
+        for (opponent, outcome) in results {
+            if outcome == Outcomes::WIN {
+                println!("      {} {} win", opponent.rating, opponent.deviation);
+            } else {
+                println!("      {} {} loss", opponent.rating, opponent.deviation);
+            }
+        }
     }
 }
 
@@ -25,18 +52,15 @@ fn add_new_player(players_database: &mut PlayersDatabase) {
 
     // players_database.add_new(name.trim().to_string());
 
-    players_database.add_existing("a".to_string(), GlickoRating{rating: 1500.0, deviation: 200.0});
-    players_database.add_existing("b".to_string(), GlickoRating{rating: 1400.0, deviation: 30.0});
-    players_database.add_existing("c".to_string(), GlickoRating{rating: 1550.0, deviation: 100.0});
-    players_database.add_existing("d".to_string(), GlickoRating{rating: 1700.0, deviation: 300.0});
+    // players_database.add_existing("a".to_string(), GlickoRating{rating: 1500.0, deviation: 200.0});
+    // players_database.add_existing("b".to_string(), GlickoRating{rating: 1400.0, deviation: 30.0});
+    // players_database.add_existing("c".to_string(), GlickoRating{rating: 1550.0, deviation: 100.0});
+    // players_database.add_existing("d".to_string(), GlickoRating{rating: 1700.0, deviation: 300.0});
 }
 
 fn add_new_tournament(players_database: &mut PlayersDatabase) {
-    use skillratings::{
-        glicko::{glicko, glicko_rating_period, GlickoConfig, GlickoRating},
-        Outcomes,
-    };
 
+    let mut wtr = Writer::from_path("proba.csv").unwrap();
     let mut no_of_players_string = String::new();
 
     print!("How many players attended? ");
@@ -108,14 +132,138 @@ fn add_new_tournament(players_database: &mut PlayersDatabase) {
         let new_glicko = glicko_rating_period(&(player_glicko.unwrap()), &results, &config);
 
         println!("{} {}", new_glicko.rating, new_glicko.deviation);
+
+        wtr.write_record(&[name_of_the_player.trim(), &new_glicko.rating.round().to_string(), &new_glicko.deviation.round().to_string()]);
+    }
+}
+
+fn add_set_of_tournament_games_singles(players_database: &mut PlayersDatabase) {
+    let mut name_of_the_player = String::new();
+    let mut name_of_the_opponent = String::new();
+    let mut outcome = String::new();
+
+    loop {
+        name_of_the_player = String::new();
+        name_of_the_opponent = String::new();
+        print!("Name of the player: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut name_of_the_player)
+            .expect("Failed to read line");
+
+        if name_of_the_player.trim() == "exit" {
+            break;
+        }
+
+        print!("Name of the opponent: ");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut name_of_the_opponent)
+            .expect("Failed to read line");
+
+        if !players_database.find_player(name_of_the_player.trim().to_string()) {
+            players_database.add_new(name_of_the_player.trim().to_string())
+        }
+        if !players_database.find_player(name_of_the_opponent.trim().to_string()) {
+            players_database.add_new(name_of_the_opponent.trim().to_string())
+        }
+
+        outcome = String::new();
+        while outcome.trim() != "win" && outcome.trim() != "lose" {
+            outcome = String::new();
+
+            print!("What was the outcome?(from the player's perspective) ");
+            io::stdout().flush().unwrap();
+            io::stdin()
+                .read_line(&mut outcome)
+                .expect("Failed to read line");
+        }
+
+        let player_glicko = players_database.get_player_data(name_of_the_player.trim().to_string());
+        let opponent_glicko = players_database.get_player_data(name_of_the_opponent.trim().to_string());
+        
+        if outcome.trim() == "win" {
+            players_database.add_new_result(name_of_the_player.trim().to_string(), opponent_glicko.unwrap(), Outcomes::WIN);
+            players_database.add_new_result(name_of_the_opponent.trim().to_string(), player_glicko.unwrap(), Outcomes::LOSS);
+        } else {
+            players_database.add_new_result(name_of_the_player.trim().to_string(), opponent_glicko.unwrap(), Outcomes::LOSS);
+            players_database.add_new_result(name_of_the_opponent.trim().to_string(), player_glicko.unwrap(), Outcomes::WIN);
+        }
+    }
+}
+
+fn add_set_of_tournament_games_doubles(players_database: &mut PlayersDatabase, teams_database: &mut TeamsDatabase) {
+    let mut rdr = Reader::from_path("data/input_doubles_september.csv").unwrap();
+    for result in rdr.records() {
+        let record = result.unwrap();
+        if !players_database.find_player(record.get(0).unwrap().to_string()) {
+            players_database.add_new(record.get(0).unwrap().to_string());
+        }
+        if !players_database.find_player(record.get(1).unwrap().to_string()) {
+            players_database.add_new(record.get(1).unwrap().to_string());
+        }
+        if !players_database.find_player(record.get(2).unwrap().to_string()) {
+            players_database.add_new(record.get(2).unwrap().to_string());
+        }
+        if !players_database.find_player(record.get(3).unwrap().to_string()) {
+            players_database.add_new(record.get(3).unwrap().to_string());
+        }
+
+        let team1_player1_glicko = players_database.get_player_data(record.get(0).unwrap().to_string());
+        let team1_player2_glicko = players_database.get_player_data(record.get(1).unwrap().to_string());
+        let team2_player1_glicko = players_database.get_player_data(record.get(2).unwrap().to_string());
+        let team2_player2_glicko = players_database.get_player_data(record.get(3).unwrap().to_string());
+
+        if !teams_database.find_team(record.get(0).unwrap().to_string(), record.get(1).unwrap().to_string()) {
+            teams_database.add_new(record.get(0).unwrap().to_string(), team1_player1_glicko.unwrap(), record.get(1).unwrap().to_string(), team1_player2_glicko.unwrap());
+        }
+        if !teams_database.find_team(record.get(2).unwrap().to_string(), record.get(3).unwrap().to_string()) {
+            teams_database.add_new(record.get(2).unwrap().to_string(), team2_player1_glicko.unwrap(), record.get(3).unwrap().to_string(), team2_player2_glicko.unwrap());
+        }
+
+        let team1_glicko = teams_database.get_team_glicko(record.get(0).unwrap().to_string(), record.get(1).unwrap().to_string());
+        let team2_glicko = teams_database.get_team_glicko(record.get(2).unwrap().to_string(), record.get(3).unwrap().to_string());
+
+        if record.get(4).unwrap() == "win" {
+            teams_database.add_new_result(record.get(0).unwrap().to_string(), record.get(1).unwrap().to_string(), team2_glicko.unwrap(), Outcomes::WIN);
+            teams_database.add_new_result(record.get(2).unwrap().to_string(), record.get(3).unwrap().to_string(), team1_glicko.unwrap(), Outcomes::LOSS);
+        } else {
+            teams_database.add_new_result(record.get(0).unwrap().to_string(), record.get(1).unwrap().to_string(), team2_glicko.unwrap(), Outcomes::LOSS);
+            teams_database.add_new_result(record.get(2).unwrap().to_string(), record.get(3).unwrap().to_string(), team1_glicko.unwrap(), Outcomes::WIN);
+        }
+    }
+
+
+}
+
+fn calculate_players_glicko(players_database: &PlayersDatabase) {
+    let mut wtr = Writer::from_path("data/output.csv").unwrap();
+    let players_list = players_database.list();
+    for (name, glicko, results) in players_list {
+        let config = GlickoConfig::new();
+        let new_glicko = glicko_rating_period(&glicko, &results, &config);
+
+        wtr.write_record(&[name.trim(), &new_glicko.rating.round().to_string(), &new_glicko.deviation.round().to_string()]);
+    }
+}
+
+fn calculate_teams_glicko(teams_database: &TeamsDatabase, players_database: &PlayersDatabase) {
+    let mut wtr = Writer::from_path("data/output_doubles.csv").unwrap();
+    let teams_list = teams_database.list();
+    for (name1, name2, glicko, results) in teams_list {
+        let config = GlickoConfig::new();
+        let new_glicko = glicko_rating_period(&glicko, &results, &config);
+
+        wtr.write_record(&[name1.trim(), name2.trim(), &new_glicko.rating.round().to_string(), &new_glicko.deviation.round().to_string()]);
     }
 }
 
 fn main() {
     let mut players_database: PlayersDatabase = PlayersDatabase::new(vec![]);
+    let mut teams_database: TeamsDatabase = TeamsDatabase::new(vec![]);
 
     loop {
-        let options: Vec<&str> = vec!["List all players", "Add new player", "Add new tournament", "Exit"];
+        let options: Vec<&str> = vec!["List all players", "List all teams", "Add new player", "Add new tournament", "Add Singles tournament games", "Read Doubles games from csv", "Calculate players Glicko!", "Calculate teams Glicko!", "Exit"];
 
         let ans: Result<&str, InquireError> = Select::new("Input a command: ", options).prompt();
 
@@ -124,8 +272,13 @@ fn main() {
             Ok(choice) => {
                 match choice {
                     "List all players" => list_all_players(&players_database),
+                    "List all teams" => list_all_teams(&teams_database),
                     "Add new player" => add_new_player(&mut players_database),
                     "Add new tournament" => add_new_tournament(&mut players_database),
+                    "Add Singles tournament games" => add_set_of_tournament_games_singles(&mut players_database),
+                    "Read Doubles games from csv" => add_set_of_tournament_games_doubles(&mut players_database, &mut teams_database),
+                    "Calculate players Glicko!" => calculate_players_glicko(&players_database),
+                    "Calculate teams Glicko!" => calculate_teams_glicko(&teams_database, &players_database),
                     "Exit" => break,
                     &_ => break,
                 }
